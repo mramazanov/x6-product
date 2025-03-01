361f7b2c27ca3dcc680ca1859ca0947c6f06e939
Admin <mramazanov@outlook.com>
2025-03-01 09:27:45 +0300

    init

---
+++ src/main/java/com/javajabka/x6_product/X6ProductApplication.java
@@ -0,0 +1,13 @@
+package com.javajabka.x6_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X6ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(X6ProductApplication.class, args);
+	}
+
+}
